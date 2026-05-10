24986964cb1a02b8da1cea65f0c43f9fd885354b
Programmer Blog <programmerblog.net@gmail.com>
2026-05-10 23:02:19 +0300

    ci: add automated php deployment to ec2 on port 8082

--- php-app/index.php
+++ php-app/index.php
@@ -1,4 +1,5 @@
 <?php
-    echo "<h1>PHP is running in Docker and deployed with Github Actions!</h1>";
-    echo "Current Time: " . date('Y-m-d H:i:s');
+    echo "<h1>🚀 PHP app updated and redeployed via Docker + GitHub Actions</h1>";
+    echo "Current Server Time: " . date('Y-m-d H:i:s');
+    echo "<p>Deployment test successful ✔</p>";
 ?>
