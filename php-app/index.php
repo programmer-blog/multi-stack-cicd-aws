492df5b998188997f193d70851973ad9a65b2416
Programmer Blog <programmerblog.net@gmail.com>
2026-05-10 17:07:15 +0300

    feat(php): add apache-php app and ci workflow

---
+++ php-app/index.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h1>PHP is running in Docker and deployed with Github Actions!</h1>";
+    echo "Current Time: " . date('Y-m-d H:i:s');
+?>
--- python-app/app.py
+++ python-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Python Flask is running in Docker and deployed with Github Actions!!"
+    return "Python Flask is running in Docker and deployed with Github Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
